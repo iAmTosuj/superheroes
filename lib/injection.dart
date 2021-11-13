d31f2fd226c2425cd744205fa328842ff02b7063
Tosuj <sanches00799@gmail.com>
2021-11-13 13:46:54 +0300

    feat(main_page): add cubit to di

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,17 +1,27 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:superheroes/blocs/main_bloc/main_cubit.dart';
+import 'package:superheroes/repository/main_page/main_page_repository_impl.dart';
 import 'package:superheroes/resources/base_url.dart';
 
 class DependenciesInitializer {
   static Future<void> initializeDependencies() async {
     Dio _createMainClient() {
-      final client = Dio(BaseOptions(baseUrl: '${ResBaseUrl.main}/'));
+      final token = dotenv.env['SUPERHERO_TOKEN'];
+
+      final client = Dio(BaseOptions(baseUrl: '${ResBaseUrl.main}/$token'));
 
       return client;
     }
 
     final _mainClient = _createMainClient();
+    // cubits
+    final _mainCubit =
+        MainCubit(mainRepository: MainPageRepositoryImpl(client: _mainClient));
 
     Get.put(_mainClient);
+
+    Get.put(_mainCubit);
   }
 }
