5697c313f721721ceb78e1af6c2ff5e89c561c18
Tosuj <sanches00799@gmail.com>
2021-11-16 21:56:18 +0300

    feat: add search with debounce

--- lib/blocs/main_bloc/main_state.dart
+++ lib/blocs/main_bloc/main_state.dart
@@ -3,19 +3,23 @@ part of 'main_cubit.dart';
 class MainState {
   final PageStatus status;
   final List<SuperheroInfo> superhero;
+  final bool haveSearchText;
 
   MainState({
-    this.status = PageStatus.loading,
+    required this.status,
+    this.haveSearchText = false,
     this.superhero = const [],
   });
 
   MainState copyWith({
     PageStatus? status,
     List<SuperheroInfo>? superhero,
+    bool? haveSearchText,
   }) {
     return MainState(
       status: status ?? this.status,
       superhero: superhero ?? this.superhero,
+      haveSearchText: haveSearchText ?? this.haveSearchText,
     );
   }
 }
